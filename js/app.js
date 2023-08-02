c49877cd6b93504a3ce15c8c545ce6ce2a1a6573
Wladys LELONG <wladys.lelong@gmail.com>
2023-08-02 16:20:56 +0200

    correction menu js et strong articles

--- js/app.js
+++ js/app.js
@@ -9,3 +9,17 @@ for (let i=0; i< cartes.length; i++) {
     let angle = ((Math.PI * 2) / cartes.length)*i;
     positionCercle(cartes[i], angle);
 }
+
+
+const deroulant = document.getElementById("deroulant");
+console.log(deroulant);
+const sousmenu = document.getElementById("sous-menu");
+console.log(sousmenu);
+
+let classes = sousmenu.classList;
+console.log(classes);
+
+deroulant.addEventListener("click", function () {
+    sousmenu.classList.toggle("visible");
+    console.log("clic reussi");
+});
